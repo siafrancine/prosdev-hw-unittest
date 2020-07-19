a7a3cc0c2d8ec32783f7165201ffbbe4b0329a84
siafrancine <47266577+siafrancine@users.noreply.github.com>
2020-07-19 23:33:13 +0800

    francine

--- src/index.js
+++ src/index.js
@@ -1,7 +1,46 @@
 const functions = {
   gradeClassifier: (grade) => {
     // TODO: Write code here
+    var letterGrade;
+
+    switch(true)
+    {
+      case grade >= 95 && grade <= 100:
+        letterGrade =  "O - Outstanding"
+        break;
+
+      case grade >= 90 && grade < 95:
+        letterGrade = "V - Very Good"
+        break;
+      
+      case grade >= 85 && grade < 90:
+        letterGrade = "G - Good"
+        break;
+
+      case grade >= 80 && grade < 85:
+        letterGrade = "S - Satisfactory"
+        break;
+
+      case grade >= 75 && grade < 80:
+        letterGrade = "N - Needs Improvement"
+        break;
+      
+      case grade < 75:
+        letterGrade = "D - Did not Meet Expectation"
+        break;
+      
+      default:
+        if(isNaN(grade))
+          {
+            letterGrade = "Please input a numerical grade"
+            break;
+          }
+          letterGrade = "Please enter a valid input"
+    }
+    return letterGrade;
+
   }
+  
 }
 
 module.exports = functions;
--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,86 @@
 const { gradeClassifier } = require('./index');
 
 // TODO: Write unit tests here
-describe('Scenario', () => {
-  it('When ..., then ...', () => {
+describe('Grading', () => {
+
+  it('When the teacher inputs a grade that is at least 95 but at most 100, then the grade of the student should be O - Outstanding', () => {
     // Arrange
-
+      const grade = 98
     // Act
-
+      const result = gradeClassifier(grade)
     // Assert
+      expect(result).toMatch("O - Outstanding")
+  });
+
+  it('When the teacher inputs a grade that is at least 90 but less than 95, then the grade of the student should be V - Very Good', () => {
+    // Arrange
+      const grade = 94
+    // Act
+      const result = gradeClassifier(grade)
+    // Assert
+      expect(result).toMatch("V - Very Good")
+  });
+
+  it('When the teacher inputs a grade that is at least 85 but less than 90, then the grade of the student should be G - Good', () => {
+    // Arrange
+      const grade = 86
+    // Act
+      const result = gradeClassifier(grade)
+    // Assert
+      expect(result).toMatch("G - Good")
+  });
+
+  it('When the teacher inputs a grade that is at least 80 but less than 85, then the grade of the student should be S - Satisfactory', () => {
+    // Arrange
+      const grade = 83
+    // Act
+      const result = gradeClassifier(grade)
+    // Assert
+      expect(result).toMatch("S - Satisfactory")
+  });
+
+  it('When the teacher inputs a grade that is at least 75 but less than 80, then the grade of the student should be N - Needs Improvement', () => {
+    // Arrange
+      const grade = 77
+    // Act
+      const result = gradeClassifier(grade)
+    // Assert
+      expect(result).toMatch("N - Needs Improvement")
+  });
+
+  it('When the teacher inputs a grade less than 80, then the grade of the student should be D - Did not Meet Expectation', () => {
+    // Arrange
+      const grade = 65
+    // Act
+      const result = gradeClassifier(grade)
+    // Assert
+      expect(result).toMatch("D - Did not Meet Expectation")
+  });
+
+  it('When the teacher inputs a grade less than 80, then the grade of the student should be D - Did not Meet Expectation', () => {
+    // Arrange
+      const grade = 0
+    // Act
+      const result = gradeClassifier(grade)
+    // Assert
+      expect(result).toMatch("D - Did not Meet Expectation")
+  });
+
+  it('When the teacher inputs a grade that is not a number, then it should return Please input a numerical grade', () => {
+    // Arrange
+      const grade = 'hello'
+    // Act
+      const result = gradeClassifier(grade)
+    // Assert
+      expect(result).toMatch("Please input a numerical grade")
+  });
+
+  it('When the teacher inputs a grade more than 100, it should return Please enter a valid input', () => {
+    // Arrange
+      const grade = 'hello'
+    // Act
+      const result = gradeClassifier(grade)
+    // Assert
+      expect(result).toMatch("Please input a numerical grade")
   });
 });
